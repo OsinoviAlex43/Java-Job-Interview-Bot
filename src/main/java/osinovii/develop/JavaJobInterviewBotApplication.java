3bac63cccc68e9dd57da1b0a30e17629a0e9d5e3
OsinoviAlex43 <osinovyjaleksej@gmail.com>
2025-05-08 19:17:40 +0300

    Creating a new project Java-Job-Interview-Bot

---
+++ src/main/java/osinovii/develop/JavaJobInterviewBotApplication.java
@@ -0,0 +1,13 @@
+package osinovii.develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJobInterviewBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaJobInterviewBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/osinovii/develop/JavaJobInterviewBotApplicationTests.java
@@ -0,0 +1,13 @@
+package osinovii.develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJobInterviewBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
